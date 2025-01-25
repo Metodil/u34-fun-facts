0539beae2971e47c2509ec7b8139bc2d0ca68eaa
metodil <metodil@hotmail.com>
2025-01-25 22:10:08 +0200

    ajust terraform/aws_eks_add_ons/provider.tf

--- terraform/aws_eks_add_ons/provider.tf
+++ terraform/aws_eks_add_ons/provider.tf
@@ -1,23 +1,9 @@
 terraform {
+  required_version = ">= 0.15.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 5.80.0"
     }
   }
-}
-provider "aws" {
-  region = var.aws_region
-  #  allowed_account_ids = [482167452852]
-
-  default_tags {
-    tags = local.default_tags
-  }
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-
 }
